876468d20527360b8dd491a1fb98d198cc99bcda
aman-km <87479298+aman-km@users.noreply.github.com>
2021-09-27 22:29:00 +0530

    Add files via upload

---
+++ games.js
@@ -0,0 +1,109 @@
+//this is the kernel of the arcade os to work on a simple html file
+
+
+//the specific function to work the "game : minion-rush" on the backend of the arcade os
+function minionrush(){
+	document.getElementById('minion-rush').style.display = "flex";
+	document.getElementById('h2').style.display = "none";
+	document.getElementById('spring-panda').style.display = "none";
+	document.getElementById('santa-goes-home').style.display = "none";
+	document.getElementById('hill-climb-racing').style.display = "none"; 
+	document.getElementById('jump-on-jupiter').style.display = "none";
+	document.getElementById('brave-triangle').style.display="none";
+			document.getElementById('car-speed-booster').style.display="none"; 
+
+
+}
+
+//the specific function to work the game on the backend of the arcade os
+function santagoeshome(){
+	document.getElementById('santa-goes-home').style.display = "flex";
+	document.getElementById('h2').style.display = "none";
+	document.getElementById('spring-panda').style.display = "none";
+	document.getElementById('minion-rush').style.display = "none";
+	document.getElementById('hill-climb-racing').style.display = "none"; 
+	document.getElementById('jump-on-jupiter').style.display = "none";
+		document.getElementById('brave-triangle').style.display="none";
+				document.getElementById('car-speed-booster').style.display="none"; 
+
+
+
+}
+
+//the specific function to work the game on the backend of the arcade os
+function springpanda(){
+	document.getElementById('spring-panda').style.display = "flex";
+	document.getElementById('h2').style.display = "none";
+	document.getElementById('santa-goes-home').style.display = "none";
+	document.getElementById('minion-rush').style.display = "none";
+	document.getElementById('hill-climb-racing').style.display = "none"; 
+	document.getElementById('jump-on-jupiter').style.display = "none";
+		document.getElementById('brave-triangle').style.display="none";
+				document.getElementById('car-speed-booster').style.display="none"; 
+
+
+}
+
+//the specific function to work the game on the backend of the arcade os
+function hillclimbracing(){
+	document.getElementById('spring-panda').style.display = "none";
+	document.getElementById('h2').style.display = "none";
+	document.getElementById('santa-goes-home').style.display = "none";
+	document.getElementById('minion-rush').style.display = "none";
+	document.getElementById('hill-climb-racing').style.display = "flex";
+	document.getElementById('jump-on-jupiter').style.display = "none"; 
+		document.getElementById('brave-triangle').style.display="none";
+				document.getElementById('car-speed-booster').style.display="none"; 
+
+	 
+
+}
+//the specific function to work the game on the backend of the arcade os
+function jumponjupiter(){
+	document.getElementById('spring-panda').style.display = "none";
+	document.getElementById('h2').style.display = "none";
+	document.getElementById('santa-goes-home').style.display = "none";
+	document.getElementById('minion-rush').style.display = "none";
+	document.getElementById('hill-climb-racing').style.display = "none";
+	document.getElementById('jump-on-jupiter').style.display = "flex";	 
+		document.getElementById('brave-triangle').style.display="none";
+				document.getElementById('car-speed-booster').style.display="none"; 
+
+
+
+}
+
+function bravetriangle(){
+
+	document.getElementById('spring-panda').style.display = "none";
+	document.getElementById('h2').style.display = "none";
+	document.getElementById('santa-goes-home').style.display = "none";
+	document.getElementById('minion-rush').style.display = "none";
+	document.getElementById('hill-climb-racing').style.display = "none";
+	document.getElementById('jump-on-jupiter').style.display = "none";	
+	document.getElementById('brave-triangle').style.display="flex";
+		document.getElementById('car-speed-booster').style.display="none"; 
+
+
+}
+
+
+function carspeedbooster(){
+
+	document.getElementById('spring-panda').style.display = "none";
+	document.getElementById('h2').style.display = "none";
+	document.getElementById('santa-goes-home').style.display = "none";
+	document.getElementById('minion-rush').style.display = "none";
+	document.getElementById('hill-climb-racing').style.display = "none";
+	document.getElementById('jump-on-jupiter').style.display = "none";	
+	document.getElementById('brave-triangle').style.display="none";
+	document.getElementById('car-speed-booster').style.display="flex"; 
+
+
+
+}
+
+
+
+
+
